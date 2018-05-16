1b9b0cb4875f12d4e51bf33c3509c037c6b7618b
Ángel Vásquez <angel.vasquez.unam@gmail.com>
2018-05-16 02:02:00 -0500

    Se agrega función para detectar si se repiten V - L - D

--- Romanos.java
+++ Romanos.java
@@ -6,7 +6,9 @@ public class Romanos {
 
   public static String letrasRomanos = "IVXLCDM";
   public static int[] valoresRomanos = { 1, 5, 10, 50, 100, 500, 1000 };
+  public static char[] valoresError = { 'V', 'L', 'D' };
   
+
   public static String numRomUser = new String();
   public static char[] arrayRom = {};
   public static String[] arrayGroupRom = {};
@@ -17,11 +19,11 @@ public class Romanos {
     System.out.println("Bienvenido");
     System.out.print("Por favor ingrese un número romano: ");
     numRomUser = entrada.nextLine();
-    index();
-    // repeat();
+    // valueOfLetter();
+    repeat();
   }
 
-  public static void index() {
+  public static void valueOfLetter() {
     arrayRom = numRomUser.toCharArray();
     byte i = 0;
     for(char a : arrayRom) {
@@ -42,17 +44,22 @@ public class Romanos {
 
         if( arrayRom[i] == arrayRom [i + 1] ) {
 
+          if( arrayRom[i] == valoresError[0] || arrayRom[i] == valoresError[1] || arrayRom[i] == valoresError[2] ) {
+            System.out.println("Error, no se pueden repetir de manera consecutiva los valores V, L o D");
+            System.exit(1);
+          }
+
           if( (i + 2) < arrayRom.length && arrayRom[i + 1] == arrayRom [i + 2] ) {
             System.out.println("Tres letras iguales: " + arrayRom[i]);
-
-            // Para evitar que itere sobre las otras dos letras.
-            i = (byte) (i + 2); 
 
             if( (i + 3) < arrayRom.length && arrayRom[i + 2] == arrayRom [i + 3] ) {
               System.out.println("Error, más  de 3 letras iguales");
               System.exit(1);
             }
-
+            
+            // Para evitar que itere sobre las otras dos letras.
+            i = (byte) (i + 1); 
+            
           } else {
             System.out.println("Dos letras iguales: " + arrayRom[i]);
           }
@@ -60,8 +67,9 @@ public class Romanos {
         } else {
           System.out.println("--");
         }
+
       }
-      
+
     } else {
       System.out.println("Sin igualdades");
       // Colocar valor de la letra que se haya ingresado
