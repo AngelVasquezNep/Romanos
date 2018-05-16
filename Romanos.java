f6216f5a5ef018e5ffe833afd6a40e832a6ad48d
Ángel Vásquez <angel.vasquez.unam@gmail.com>
2018-05-16 01:38:32 -0500

    Se crea método para buscar el valor de cada letra

--- Romanos.java
+++ Romanos.java
@@ -3,8 +3,13 @@ import java.util.Scanner;
 public class Romanos {
   
   public static Scanner entrada = new Scanner(System.in);
+
+  public static String letrasRomanos = "IVXLCDM";
+  public static int[] valoresRomanos = { 1, 5, 10, 50, 100, 500, 1000 };
+  
   public static String numRomUser = new String();
   public static char[] arrayRom = {};
+  public static String[] arrayGroupRom = {};
   public static String[] valueRomByArray = {};
   public static byte auxContador = 0;
 
@@ -12,8 +17,19 @@ public class Romanos {
     System.out.println("Bienvenido");
     System.out.print("Por favor ingrese un número romano: ");
     numRomUser = entrada.nextLine();
-    repeat();
+    index();
+    // repeat();
   }
+
+  public static void index() {
+    arrayRom = numRomUser.toCharArray();
+    byte i = 0;
+    for(char a : arrayRom) {
+      System.out.println("Valor de [" + a +"]: " +  valoresRomanos[letrasRomanos.indexOf(a)] );
+      i++;
+    }
+  }
+
 
   public static void repeat() {
     arrayRom = numRomUser.toCharArray();
@@ -23,17 +39,24 @@ public class Romanos {
       // boolean igual;
       
       for (byte i = 0; (i + 1) < arrayRom.length; i++ ) {
+
         if( arrayRom[i] == arrayRom [i + 1] ) {
-          System.out.println("Dos letras iguales: " + arrayRom[i]);
+
           if( (i + 2) < arrayRom.length && arrayRom[i + 1] == arrayRom [i + 2] ) {
             System.out.println("Tres letras iguales: " + arrayRom[i]);
 
+            // Para evitar que itere sobre las otras dos letras.
+            i = (byte) (i + 2); 
+
             if( (i + 3) < arrayRom.length && arrayRom[i + 2] == arrayRom [i + 3] ) {
               System.out.println("Error, más  de 3 letras iguales");
-              System.out.println("Salir ");
               System.exit(1);
             }
+
+          } else {
+            System.out.println("Dos letras iguales: " + arrayRom[i]);
           }
+
         } else {
           System.out.println("--");
         }
@@ -45,4 +68,14 @@ public class Romanos {
       // ToDo Verificar que sólo se ingresen letras validas
     }
   }
+
+  public static byte sustituirValor (String v) {
+    
+    return 1;
+  }
+
+  public static int sumarIguales () {
+    return 1;
+  }
+
 }
