fa078f8a9a06c88f47ea23b9673b52756947f30a
Ángel Vásquez <angel.vasquez.unam@gmail.com>
2018-05-16 01:07:58 -0500

    Reconoce letras iguales y marca error si son más de 3

--- Romanos.java
+++ Romanos.java
@@ -1,51 +1,48 @@
 import java.util.Scanner;
 
-import com.sun.tools.javac.comp.Todo;
-
-
-
-
-
-
 public class Romanos {
   
   public static Scanner entrada = new Scanner(System.in);
   public static String numRomUser = new String();
   public static char[] arrayRom = {};
   public static String[] valueRomByArray = {};
+  public static byte auxContador = 0;
 
   public static void main (String[] args) {
     System.out.println("Bienvenido");
-    System.out.println("Por favor ingrese un número romano: ");
+    System.out.print("Por favor ingrese un número romano: ");
     numRomUser = entrada.nextLine();
     repeat();
   }
-  
-  public static boolean repeat() {
+
+  public static void repeat() {
     arrayRom = numRomUser.toCharArray();
 
-    // case 1 => Sin coincidencias => Guardar en valueRom[0]
     if (arrayRom.length > 1) {
-        
+      
+      // boolean igual;
+      
       for (byte i = 0; (i + 1) < arrayRom.length; i++ ) {
-        
-      }
+        if( arrayRom[i] == arrayRom [i + 1] ) {
+          System.out.println("Dos letras iguales: " + arrayRom[i]);
+          if( (i + 2) < arrayRom.length && arrayRom[i + 1] == arrayRom [i + 2] ) {
+            System.out.println("Tres letras iguales: " + arrayRom[i]);
 
+            if( (i + 3) < arrayRom.length && arrayRom[i + 2] == arrayRom [i + 3] ) {
+              System.out.println("Error, más  de 3 letras iguales");
+              System.out.println("Salir ");
+              System.exit(1);
+            }
+          }
+        } else {
+          System.out.println("--");
+        }
+      }
+      
     } else {
+      System.out.println("Sin igualdades");
       // Colocar valor de la letra que se haya ingresado
       // ToDo Verificar que sólo se ingresen letras validas
     }
-
-    // case 2 => Una coincidencia => Guardar en valueRom[1]
-    // arrayRom[i] == arrayRom[i+1]
-    // case 3 => Dos coincidencia => Guardar en valueRom[2]
-    // arrayRom[i] == arrayRom[i+1] == arrayRom[i+2]
-    if( arrayRom[0] == arrayRom[1] ) {
-      System.out.println("Iguales");
-    } else {
-      System.out.println("Diferentes");
-    }
-    
-    return 1 > 2;
   }
 }
