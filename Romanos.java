0916a790174fc82bb2c9b92a4bb2596552e3ea2c
Ángel Vásquez <angel.vasquez.unam@gmail.com>
2018-05-16 19:43:21 -0500

    Los indices de asignación se recorren con éxito

--- Romanos.java
+++ Romanos.java
@@ -10,7 +10,7 @@ public class Romanos {
   
 
   public static String numRomUser = new String();
-  public static char[] arrayRom = {};
+  public static char[] arrayRomUser = {};
   // Fila: Character - Column: Veces repetido
   public static char[] arrayAgrupadosLetras = new char[20];
   public static int[] arrayAgrupadosRepetidos = new int[20];
@@ -26,66 +26,94 @@ public class Romanos {
   }
 
   public static void valueOfLetter() {
-    arrayRom = numRomUser.toCharArray();
+    arrayRomUser = numRomUser.toCharArray();
     byte i = 0;
-    for(char a : arrayRom) {
+    for(char a : arrayRomUser) {
       System.out.println("Valor de [" + a +"]: " +  valoresRomanos[letrasRomanos.indexOf(a)] );
       i++;
     }
   }
 
   public static void repeat() {
-    arrayRom = numRomUser.toCharArray();
+    arrayRomUser = numRomUser.toCharArray();
 
-    if (arrayRom.length > 1) {
+    errorLetras();
+
+    System.out.println("Repeat Initi ---------");
+
+    if (arrayRomUser.length > 1) {
       
-      // boolean igual;
-      
-      for (byte i = 0; (i + 1) < arrayRom.length; i++ ) {
+      for (byte i = 0; i < arrayRomUser.length; i++ ) {
 
-        if( arrayRom[i] == arrayRom [i + 1] ) {
+        if( (i + 1) < arrayRomUser.length && arrayRomUser[i] == arrayRomUser [i + 1] ) {
 
-          if( arrayRom[i] == valoresError[0] || arrayRom[i] == valoresError[1] || arrayRom[i] == valoresError[2] ) {
+          if( arrayRomUser[i] == valoresError[0] || arrayRomUser[i] == valoresError[1] || arrayRomUser[i] == valoresError[2] ) {
             System.out.println("Error, no se pueden repetir de manera consecutiva los valores V, L o D");
             System.exit(1);
           }
 
-          if( (i + 2) < arrayRom.length && arrayRom[i + 1] == arrayRom [i + 2] ) {
-            System.out.println("Tres letras iguales: " + arrayRom[i]);
-            asignar(arrayRom[i], 3);
+          if( (i + 2) < arrayRomUser.length && arrayRomUser[i + 1] == arrayRomUser [i + 2] ) {
+            // System.out.println("Tres letras iguales: " + arrayRomUser[i]);
+            asignar(arrayRomUser[i], 3);
 
-            if( (i + 3) < arrayRom.length && arrayRom[i + 2] == arrayRom [i + 3] ) {
+            if( (i + 3) < arrayRomUser.length && arrayRomUser[i + 2] == arrayRomUser [i + 3] ) {
               System.out.println("Error, más  de 3 letras iguales");
               System.exit(1);
             }
             
             // Para evitar que itere sobre las otras dos letras.
-            i = (byte) (i + 1);
+            // i = (byte) (i + 1);
+            i = (byte) (i + 2);
             
           } else {
-            asignar(arrayRom[i], 2);
-            System.out.println("Dos letras iguales: " + arrayRom[i]);
+            asignar(arrayRomUser[i], 2);
+            i = (byte) (i + 1);
+            // System.out.println("Dos letras iguales: " + arrayRomUser[i]);
           }
           
         } else {
-          if( !( (i + 2) < arrayRom.length) ) {
-            System.out.println("--");
-            asignar(arrayRom[i+1], 1);
-          }
+          // if( (i + 2) < arrayRomUser.length) {
+          //   System.out.println("I + 2? -----");
+          //   // asignar(arrayRomUser[ i + 1 ], 1);
+          // }
+          // else {
+          //   System.out.println("Asignar 1 sólo elemento ----- ");
+          //   asignar(arrayRomUser[i], 1);
+          // }
+          System.out.println("Asignar 1 sólo elemento ----- ");
+          asignar(arrayRomUser[i], 1);
         }
-
+        
       }
-
+      
     } else {
       System.out.println("Sólo una letra");
-      asignar(arrayRom[0], 1);
+      asignar(arrayRomUser[0], 1);
       // Colocar valor de la letra que se haya ingresado
       // ToDo Verificar que sólo se ingresen letras validas
     }
 
     listarElementos();
-
   }
+
+  public static void errorLetras () {
+
+    char[] charRom = letrasRomanos.toCharArray();
+
+    for (char letter : arrayRomUser) {
+      for (int i = 0; i < charRom.length; i++) {
+        if(letter == charRom[i]) {
+          break;
+        }
+        if( ( i + 1 ) == charRom.length ) {
+          System.out.println(" Error: Letras romanas válidas: I, V, X, L, C, D y M ");
+          System.exit(1);
+        }
+      }
+    }
+  }
+
+
   public static void listarElementos () {
     for(int i = 0; i < auxContador; i++){
       System.out.println("::::::::::::::::::::: ");
@@ -108,11 +136,11 @@ public class Romanos {
     // System.out.println("--");
     arrayAgrupadosLetras[auxContador] = letra;
     arrayAgrupadosRepetidos[auxContador] = nRepetidos;
-    System.out.println("--");
-    // System.out.println("Letra: " + arrayAgrupadosLetras[auxContador] );
-    // System.out.println("Repetidos: " + arrayAgrupadosRepetidos[auxContador] );
+    // System.out.println("--");
+    System.out.println("Letra: " + arrayAgrupadosLetras[auxContador] );
+    System.out.println("Repetidos: " + arrayAgrupadosRepetidos[auxContador] );
     auxContador++;
-    System.out.println("auxContador: " + auxContador );
+    // System.out.println("auxContador: " + auxContador );
   }
 
 }
