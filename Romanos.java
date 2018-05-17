66ac8b30598a16f240715a51a39a47c5ef68c015
Ángel Vásquez <angel.vasquez.unam@gmail.com>
2018-05-17 00:17:36 -0500

    Diferencia entre sumas pares e inpares

--- Romanos.java
+++ Romanos.java
@@ -1,3 +1,4 @@
+import java.awt.image.RescaleOp;
 import java.util.Scanner;
 
 public class Romanos {
@@ -13,8 +14,8 @@ public class Romanos {
   public static char[] arrayRomUser = {};
   public static char[] arrayAgrupadosLetras = new char[20];
   public static int[] arrayAgrupadosRepetidos = new int[20];
-  public static String[] valueRomByArray = {};
   public static int auxContador = 0;
+  public static int[] resultSustitucionValores;
 
   public static int resultadoSuma = 0;
 
@@ -23,17 +24,7 @@ public class Romanos {
     System.out.println("Bienvenido");
     System.out.print("Por favor ingrese un número romano: ");
     numRomUser = entrada.nextLine();
-    // valueOfLetter();
     repeat();
-  }
-
-  public static void valueOfLetter() {
-    arrayRomUser = numRomUser.toCharArray();
-    byte i = 0;
-    for(char a : arrayRomUser) {
-      System.out.println("Valor de [" + a +"]: " +  valoresRomanos[letrasRomanos.indexOf(a)] );
-      i++;
-    }
   }
 
   public static void repeat() {
@@ -54,7 +45,7 @@ public class Romanos {
 
           if( (i + 2) < arrayRomUser.length && arrayRomUser[i + 1] == arrayRomUser [i + 2] ) {
             // System.out.println("Tres letras iguales: " + arrayRomUser[i]);
-            asignar(arrayRomUser[i], 3);
+            asignarToArray(arrayRomUser[i], 3);
 
             if( (i + 3) < arrayRomUser.length && arrayRomUser[i + 2] == arrayRomUser [i + 3] ) {
               System.out.println("Error, más  de 3 letras iguales");
@@ -66,29 +57,69 @@ public class Romanos {
             i = (byte) (i + 2);
             
           } else {
-            asignar(arrayRomUser[i], 2);
+            asignarToArray(arrayRomUser[i], 2);
             i = (byte) (i + 1);
             // System.out.println("Dos letras iguales: " + arrayRomUser[i]);
           }
           
         } else {
-          // System.out.println("Asignar 1 sólo elemento ----- ");
-          asignar(arrayRomUser[i], 1);
+          // System.out.println(Asignar 1 sólo elemento ----- ");
+          asignarToArray(arrayRomUser[i], 1);
         }
         
       }
       
     } else {
       System.out.println("Sólo una letra");
-      asignar(arrayRomUser[0], 1);
+      asignarToArray(arrayRomUser[0], 1);
       // Colocar valor de la letra que se haya ingresado
       // ToDo Verificar que sólo se ingresen letras validas
     }
 
-    // listarElementos();
+
     sustituirValor();
 
+    System.out.println();
+    System.out.println(":::::::::::::::::::::: <Resultados> :::::::::::::::::::");
+    for (int i = 0; i < resultSustitucionValores.length; i++) {
+      System.out.println("Resultado " + (i + 1) + ": " + resultSustitucionValores[i] );
+    }
+    System.out.println(":::::::::::::::::::::: </Resultados> ::::::::::::::::::");
+    System.out.println();
+    
+    sumar();
+    
   }
+  
+  
+  public static void sumar () {
+    switch ( auxContador % 2) {
+      case 0:
+      sumarPar();
+      break;
+      default:
+      sumarInPar();
+      break;
+    }
+  }
+  
+  public static void sumarPar () {
+    System.out.println();
+    System.out.println( "Par: " );
+    System.out.println();
+  }
+  
+  public static void sumarInPar () {
+    System.out.println();
+    System.out.println( "Inpar: " );
+    System.out.println();
+  }
+
+
+
+
+
+
 
   public static void errorLetras () {
 
@@ -107,23 +138,14 @@ public class Romanos {
     }
   }
 
-
-  public static void listarElementos () {
-    System.out.println( "::::::::::::::::::::: <Los elementos> :::::::::::::::::::::" );
-    for(int i = 0; i < auxContador; i++){
-      System.out.println("Letra: " + arrayAgrupadosLetras[i]);
-      System.out.println("Valor: " + arrayAgrupadosRepetidos[i]);
-    }
-    System.out.println( "Cantidad de elementos: " + auxContador );
-    System.out.println( "::::::::::::::::::::: </Los elementos> ::::::::::::::::::::");
-  }
-
-
   public static void sustituirValor () {
+    System.out.println();
     System.out.println( "::::::::::::::::::::: <Sustituidos> :::::::::::::::::::::" );
+    resultSustitucionValores = new int[auxContador];
     for(int i = 0; i < auxContador; i++){
       System.out.println("Letra: " + arrayAgrupadosLetras[i]);
       System.out.println("Valor: " + valoresRomanos[letrasRomanos.indexOf(arrayAgrupadosLetras[i])] * arrayAgrupadosRepetidos[i] ) ;
+      resultSustitucionValores[i] = valoresRomanos[letrasRomanos.indexOf(arrayAgrupadosLetras[i])] * arrayAgrupadosRepetidos[i];
     }
     System.out.println("----------------------------------------------------------");
     System.out.println( "Cantidad de elementos: " + auxContador );
@@ -131,11 +153,7 @@ public class Romanos {
     System.out.println("");
   }
 
-  public static int sustituirValor (char letra, int repetidos) {
-    return valoresRomanos[letrasRomanos.indexOf(letra)] * repetidos;
-  }
-
-  public static void asignar (char letra, int nRepetidos) {
+  public static void asignarToArray (char letra, int nRepetidos) {
     // System.out.println("Letra: " + letra + " Repetidos: " + nRepetidos);
     // System.out.println("--");
     arrayAgrupadosLetras[auxContador] = letra;
